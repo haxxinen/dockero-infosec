fde58f2f9c51dc65c5279ad6e65be953067fcb84
Häxxi Häxxinen <32751905+haxxinen@users.noreply.github.com>
2019-01-07 00:52:27 +0100

    Weaponized PHP web shell.

--- weevely/setup.sh
+++ weevely/setup.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-apk --update --no-cache add python-dev py-pip git gcc make bash readline-dev ncurses-dev musl-dev libffi-dev openssl-dev
+apk --update --no-cache add python-dev py-pip git gcc make readline-dev ncurses-dev musl-dev libffi-dev openssl-dev
 pip install --upgrade pip
 cd && git clone https://github.com/epinna/weevely3 && cd weevely3
 pip install -r requirements.txt
