6c6886fb12f40848b96c03d3af87f20f5da59518
Häxxi Häxxinen <32751905+haxxinen@users.noreply.github.com>
2019-01-07 01:41:43 +0100

    Tool for finding exploits.

---
+++ pompem/README.md
@@ -0,0 +1,16 @@
+#### 1. About
+
+- URL: `https://github.com/rfunix/Pompem`
+- tool for finding exploits
+
+
+#### 2. Build
+```
+# docker build -t pompem .
+```
+
+
+#### 3. Example
+```
+# docker run --rm -it pompem -s nginx
+```
---
+++ pompem/setup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+set -e
+
+apk --update --no-cache add python3 git
+pip3 install --upgrade pip setuptools && rm -r /root/.cache
+cd && git clone https://github.com/rfunix/Pompem
+cd 'Pompem' && pip install -r requirements.txt
+apk del git
