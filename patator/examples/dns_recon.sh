417edad813d24fdf9c74b247944ba288c1143179
Häxxi Häxxinen <32751905+haxxinen@users.noreply.github.com>
2020-03-16 17:22:34 +0100

    Update dns_recon.sh

--- patator/examples/dns_recon.sh
+++ patator/examples/dns_recon.sh
@@ -5,5 +5,6 @@ name='FILE0.example.com'
 
 docker run --rm -it -v $list:/F0 \
 patator dns_forward name=$name 0=/F0 \
--x ignore:code=3 --max-retries=2 \
---timeout=3 --rate-limit=1 --threads=50
+-x ignore:code=3 -x ignore:mesg='SERVFAIL' \
+--max-retries=2 --timeout=3 \
+--rate-limit=1 --threads=100
