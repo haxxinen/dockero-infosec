b215077673a6b47287a1710852df11861773794b
Häxxi Häxxinen <32751905+haxxinen@users.noreply.github.com>
2020-03-27 17:15:06 +0100

    Mass DNS resolver.

--- massdns/entrypoint.sh
+++ massdns/entrypoint.sh
@@ -6,3 +6,4 @@ massdns --resolvers /nameservers.txt --processes 4 -o S -q -w /tmp/r /d 2>/dev/n
 echo -n > /r
 grep -h ' A ' /tmp/r*  >> /r 2>/dev/null
 grep -h ' AAAA ' /tmp/r* >> /r 2>/dev/null
+grep -h ' CNAME ' /tmp/r* >> /r 2>/dev/null
