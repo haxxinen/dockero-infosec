47c2923eaca7b7b11a4733b486fa54756b0328ce
Häxxi Häxxinen <32751905+haxxinen@users.noreply.github.com>
2020-03-28 13:51:58 +0100

    Mass DNS resolver.

--- massdns/README.md
+++ massdns/README.md
@@ -11,6 +11,13 @@
 
 #### 3. Run
 ```
+# cat > /tmp/domains.txt << EOF
+mail.google.com
+docs.google.com
+ipv6.google.com
+void.google.com
+> EOF
 # touch /tmp/results.txt
 # docker run --rm -it -v /tmp/results.txt:/r -v /tmp/domains.txt:/d massdns
+# cat /tmp/results.txt
 ```
--- massdns/entrypoint.sh
+++ massdns/entrypoint.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-massdns --resolvers /nameservers.txt --processes 4 -o S -q -w /tmp/r /d 2>/dev/null
-echo -n > /r
-grep -h ' A ' /tmp/r*  >> /r 2>/dev/null
-grep -h ' AAAA ' /tmp/r* >> /r 2>/dev/null
-grep -h ' CNAME ' /tmp/r* >> /r 2>/dev/null
+massdns --resolvers /nameservers.txt --processes 4 -o S -q -w /tmp/r1 /d 2>/dev/null
+massdns --resolvers /nameservers.txt --processes 4 -t AAAA -o S -q -w /tmp/r2 /d 2>/dev/null
+echo -n > /r && grep -hE ' CNAME | A | AAAA ' /tmp/r* | sort -u >> /r 2>/dev/null
