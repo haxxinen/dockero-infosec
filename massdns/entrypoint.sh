8cce1f0065b0973e4196390f83c0cea1017320aa
Häxxi Häxxinen <32751905+haxxinen@users.noreply.github.com>
2020-04-13 19:15:53 +0200

    Update entrypoint.sh

--- massdns/entrypoint.sh
+++ massdns/entrypoint.sh
@@ -4,4 +4,4 @@ set -e
 
 massdns --resolvers /nameservers.txt --processes `nproc` -o S -q -w /tmp/r1 /d 2>/dev/null
 massdns --resolvers /nameservers.txt --processes `nproc` -t AAAA -o S -q -w /tmp/r2 /d 2>/dev/null
-echo -n > /tmp/massdns.txt && grep -hE ' CNAME | A | AAAA ' /tmp/r* | sort -u >> /tmp/massdns.txt 2>/dev/null
+echo -n > $results && grep -hE ' CNAME | A | AAAA ' /tmp/r* | sort -u >> $results 2>/dev/null
