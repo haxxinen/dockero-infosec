fddbb46e6b3c723d340097bcaf397f79c9436f1e
haxxinen <32751905+haxxinen@users.noreply.github.com>
2020-06-02 23:40:21 +0200

    Update setup.sh

--- nmap/setup.sh
+++ nmap/setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 set -e
-apk --update --no-cache add nmap nmap-scripts nmap-nselibs git curl python3
+apk --update --no-cache add nmap nmap-scripts nmap-nselibs git curl python3 py3-pip
 
 pip3 install --upgrade pip setuptools
 pip install python-nmap
