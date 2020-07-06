f6d44ae9048abe26b36340bbdf37301362be3f33
haxxinen <32751905+haxxinen@users.noreply.github.com>
2020-07-06 21:52:21 +0200

    Update setup.sh

--- patator/setup.sh
+++ patator/setup.sh
@@ -10,7 +10,7 @@ git-core cmake xsltproc libx11-dev libxext-dev libxinerama-dev \
 libxcursor-dev libxdamage-dev libxv-dev libxkbfile-dev libasound2-dev libcups2-dev \
 libxml2 libxml2-dev libxrandr-dev libxi-dev libgstreamer-plugins-base1.0-dev libusb-dev libusb-1.0-0-dev
 
-pip install pyopenssl pyasn1 impacket pysqlcipher3 pyopenssl mysqlclient==1.3.12
+pip3 install pyopenssl pyasn1 impacket pysqlcipher3 pyopenssl mysqlclient==1.3.12
 
 git clone https://github.com/FreeRDP/FreeRDP/ /tmp/FreeRDP
 cd /tmp/FreeRDP && cmake -DCMAKE_BUILD_TYPE=Debug -DWITH_SSE2=ON . && make && make install
