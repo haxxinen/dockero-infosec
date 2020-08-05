b95e33202e228272636f2cbd7bbf5a0463bb465f
haxxinen <32751905+haxxinen@users.noreply.github.com>
2020-08-05 11:00:51 +0200

    Update setup.sh

--- eyewitness/setup.sh
+++ eyewitness/setup.sh
@@ -6,6 +6,5 @@ apt-get update -qq && apt-get install -yqq --no-install-recommends --no-install-
 git wget build-essential ca-certificates apt-utils python3 python3-dev python3-pip x11-utils
 
 git clone https://github.com/ChrisTruncer/EyeWitness /tmp/EyeWitness
-cd /tmp/EyeWitness/setup
+cd /tmp/EyeWitness/Python/setup
 bash setup.sh
-
