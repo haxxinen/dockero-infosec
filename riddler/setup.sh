f0ecd5c90f0a553100a7eb693f0adaa651e3b171
haxxinen <32751905+haxxinen@users.noreply.github.com>
2020-09-28 10:27:39 +0200

    Update setup.sh

--- riddler/setup.sh
+++ riddler/setup.sh
@@ -2,8 +2,9 @@
 
 set -e
 
-apk --update --no-cache add python3 python3-dev
-pip3 install --upgrade pip setuptools && rm -r /root/.cache
+apk --update --no-cache add python3 python3-dev py3-pip
 
+pip3 install --upgrade pip setuptools && rm -r /root/.cache
 pip3 install requests
+
 cd / && wget https://riddler.io/static/tools/riddler-cli.py
