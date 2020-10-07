7beb3d614e404b0302fe3796b96b5d8ad628e803
haxxinen <32751905+haxxinen@users.noreply.github.com>
2020-10-07 10:39:51 +0200

    Update setup.sh

--- wfuzz/setup.sh
+++ wfuzz/setup.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-apk --update --no-cache add python3 python3-dev curl-dev gcc musl-dev git
+apk --update --no-cache add python3 python3-dev py3-pip curl-dev gcc musl-dev git
 pip3 install --upgrade pip setuptools && rm -r /root/.cache
 cd && git clone https://github.com/xmendez/wfuzz && cd wfuzz
 pip install -r requirements.txt
