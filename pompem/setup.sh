d3b9945b63b42364199a15b2e6e7636b3d2273da
haxxinen <32751905+haxxinen@users.noreply.github.com>
2020-11-23 11:07:25 +0100

    Update setup.sh

--- pompem/setup.sh
+++ pompem/setup.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-apk --update --no-cache add python3 git
+apk --update --no-cache add python3 py3-pip git
 pip3 install --upgrade pip setuptools && rm -r /root/.cache
 cd && git clone https://github.com/rfunix/Pompem
 cd 'Pompem' && pip install -r requirements.txt
