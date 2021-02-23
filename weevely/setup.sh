73b4d5ecba57ba42b57940a9b27dac9a72212a66
userx <32751905+haxxinen@users.noreply.github.com>
2021-02-23 13:11:51 +0100

    Updated dependencies for python3.

--- weevely/setup.sh
+++ weevely/setup.sh
@@ -2,7 +2,10 @@
 
 set -e
 
-apk --update --no-cache add python-dev py-pip git gcc make readline-dev ncurses-dev musl-dev libffi-dev openssl-dev
+apk --update --no-cache add python3-dev py3-pip git gcc \
+make readline-dev ncurses-dev musl-dev libffi-dev openssl-dev rust cargo
+
 pip install --upgrade pip
+
 cd && git clone https://github.com/epinna/weevely3 && cd weevely3
-pip install -r requirements.txt
+pip3 install -r requirements.txt
