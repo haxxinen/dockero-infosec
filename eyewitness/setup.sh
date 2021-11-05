439dd2f77a51a8ae327163642a770679bf2ac1ab
userx <32751905+haxxinen@users.noreply.github.com>
2021-11-05 10:34:44 +0100

    Updated missing dependency.

--- eyewitness/setup.sh
+++ eyewitness/setup.sh
@@ -3,7 +3,7 @@
 set -e
 
 apt-get update -qq && apt-get install -yqq --no-install-recommends --no-install-suggests \
-git wget build-essential ca-certificates apt-utils python3 python3-dev python3-pip x11-utils
+git wget build-essential ca-certificates apt-utils python3 python3-dev python3-pip x11-utils python3-setuptools
 
 git clone https://github.com/ChrisTruncer/EyeWitness /tmp/EyeWitness
 cd /tmp/EyeWitness/Python/setup
