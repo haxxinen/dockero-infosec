ad36c69aad4414f1696bd7a6ce3bae1771c8191e
haxxinen <32751905+haxxinen@users.noreply.github.com>
2021-12-17 18:55:18 +0100

    Update make_configs.sh

--- freedome/configs/vpn_certs/make_configs.sh
+++ freedome/configs/vpn_certs/make_configs.sh
@@ -4,7 +4,7 @@
 mkdir ../countries
 cp fsvpn.conf fsvpn.conf.back
 
-endpoints=`grep 'dns' clp-beta-prov.servers.json | grep -oE freedome.*.net`
+endpoints=`grep 'dns' servers.json | grep -oE freedome.*.net`
 for dns in $endpoints; do
 	config=`echo -n $dns | sed 's/.freedome-vpn.net//g'`
 	sed -i "s/^remote.*.net/remote $dns/" fsvpn.conf
