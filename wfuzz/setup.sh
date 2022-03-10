61f2a3ff62c2e2a0f1d2b55c88a92e75f1dd84b1
haxxinen <32751905+haxxinen@users.noreply.github.com>
2022-03-10 15:44:35 +0100

    Update setup.sh

--- wfuzz/setup.sh
+++ wfuzz/setup.sh
@@ -5,7 +5,7 @@ set -e
 apk --update --no-cache add python3 python3-dev py3-pip curl-dev gcc musl-dev git
 pip3 install --upgrade pip setuptools && rm -r /root/.cache
 cd && git clone https://github.com/xmendez/wfuzz && cd wfuzz
-pip3 install -r requirements.txt
+pip3 install -r requirements.txt 2>/dev/null
 pip3 install shodan
 python3 setup.py install
 apk del git
