9b9bfa4d1d8da49a6d591d210b53c74c8888e782
haxxinen <32751905+haxxinen@users.noreply.github.com>
2023-02-23 12:04:16 +0100

    Update setup.sh

--- wfuzz/setup.sh
+++ wfuzz/setup.sh
@@ -5,6 +5,8 @@ set -e
 apk --update --no-cache add python3 python3-dev py3-pip curl-dev gcc musl-dev git
 pip3 install --upgrade pip setuptools && rm -r /root/.cache
 cd && git clone https://github.com/xmendez/wfuzz && cd wfuzz
+sed -i 's/pyparsing>=2.4\*/pyparsing>=2\.4\.2/g' setup.py # fix dependency issue
+
 pip3 install -r requirements.txt --ignore-installed
 pip3 install shodan
 python3 setup.py install
