8427dd65c6d0a974c59e21893b8283062f2bd1df
haxxinen <32751905+haxxinen@users.noreply.github.com>
2023-03-30 15:03:47 +0200

    Create entrypoint.sh

---
+++ chisel/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+set -e
+
+win='chisel_windows64'
+lin='chisel_linux64'
+mac='chisel_macos64'
+
+env GOOS=windows GOARCH=amd64 go build -o /tmp/$win.exe
+env GOOS=linux GOARCH=amd64 go build -o /tmp/$lin
+env GOOS=darwin GOARCH=amd64 go build -o /tmp/$mac
+
+env GOOS=windows GOARCH=amd64 garble -tiny -literals -seed=random build -o /tmp/$win"_obf.exe" -ldflags="-w -s"
+env GOOS=linux GOARCH=amd64 garble -tiny -literals -seed=random build -o /tmp/$lin"_obf" -ldflags="-w -s"
+env GOOS=darwin GOARCH=amd64 garble -tiny -literals -seed=random build -o /tmp/$mac"_obf" -ldflags="-w -s"
+# alternative obfuscation: unixpickle/gobfuscate
+
+rm -r /tmp/garble-shared*
+echo "Successfully built executables in /tmp folder!"
