e7f28478b9895cbc7f3df7f7f57c747a01709b14
haxxinen <32751905+haxxinen@users.noreply.github.com>
2023-12-14 22:33:45 +0100

    Update setup.sh

--- scrying/setup.sh
+++ scrying/setup.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-apt-get update && apt-get install wget chromium-browser -yqq
+apt-get update && apt-get install wget -yqq
 cd /tmp
 
 # install scrying & depends
 wget https://github.com/nccgroup/scrying/releases/download/v0.9.2/scrying_0.9.2_amd64.deb
 dpkg -i scrying_0.9.2_amd64.deb 2>/dev/null
-apt-get install -f
+apt-get install -f -yqq
 
 # fix libssl depends ¯\_(ツ)_/¯
 wget http://nz2.archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2_amd64.deb
