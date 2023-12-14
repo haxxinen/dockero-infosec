15ebed9ab3521cd633e986e259e3987b824c886f
haxxinen <32751905+haxxinen@users.noreply.github.com>
2023-12-14 22:08:13 +0100

    Update setup.sh

--- scrying/setup.sh
+++ scrying/setup.sh
@@ -10,7 +10,7 @@ wget https://github.com/nccgroup/scrying/releases/download/v0.9.2/scrying_0.9.2_
 dpkg -i scrying_0.9.2_amd64.deb
 apt-get install -f
 
-# install old libssl ¯\_(ツ)_/¯
+# fix libssl depends ¯\_(ツ)_/¯
 wget http://nz2.archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2_amd64.deb
 dpkg -i libssl1.1_1.1.1f-1ubuntu2_amd64.deb
 
