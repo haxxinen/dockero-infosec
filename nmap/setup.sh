40d41242ca2570f7f56ebe971624ef1a53e394e5
haxxinen <32751905+haxxinen@users.noreply.github.com>
2024-03-04 13:17:36 +0100

    Update setup.sh

--- nmap/setup.sh
+++ nmap/setup.sh
@@ -2,4 +2,5 @@
 
 set -e
 apk --update --no-cache add nmap nmap-scripts nmap-nselibs git curl python3 py3-pip py3-setuptools
+rm /usr/lib/python3*/EXTERNALLY-MANAGED # fix 'This environment is externally managed' error
 pip install python-nmap
